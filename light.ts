7a709cf48101ac96a13337ff7759d3aab54171c7
Evgeniy <wek@bk.ru>
2025-04-12 00:50:07 +0300

    init

---
+++ light.ts
@@ -0,0 +1,11 @@
+import {TypePalette} from "./TypePalette"
+
+
+export const light: TypePalette = {
+    mode: "light",
+    palette: {
+        primary: "white",
+        secondary: "#cccccc",
+        danger: "green",
+    }
+}
