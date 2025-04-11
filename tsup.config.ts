447a315a44734ad1f08dea2c547b3f4e0f3a3b04
Evgeniy <wek@bk.ru>
2025-04-12 01:55:32 +0300

    add ci b

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entryPoints: ['src/index.ts'],
+  entryPoints: ['src/index.tsx'],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
